f2957cb78b74d60e9e9aa19aae327a2aa6ee6d5f
Innectic <innectic@gmail.com>
2016-11-05 16:45:04 -0700

    Remove testing file

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,16 +1,1 @@
-const redis = require("redis");
-import { Config } from "../config";
-
-const config = new Config().config;
-
-const client = redis.createClient(config.redis);
-
-export class Redis {
-    set(key: string, value: string) {
-        client.set(key, value);
-    }
-
-    get(key: string) {
-        return client.get(key);
-    }
-}
+import * as Promise from "bluebird";
