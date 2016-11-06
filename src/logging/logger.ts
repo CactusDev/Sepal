fba9d248a8adbb916168e6a39b31b8f4ee77802e
Innectic <innectic@gmail.com>
2016-11-06 08:14:07 -0800

    Implement sentry

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,6 +1,18 @@
 
 const colors = require("colors/safe");
 
+import { Config } from "../config";
+const config = new Config().config;
+
+const raven = require("raven");
+
+let client: any = null;
+
+if (config.env === "prod" && config.sentry.enabled) {
+    client = new raven.Client(config.sentry.url);
+}
+
+
 export function info(message: string) {
     console.log(colors.green("INFO: ") + colors.white(message));
 }
@@ -11,6 +23,9 @@ export function warning(message: string) {
 
 export function error(message: string) {
     console.log(colors.red("ERROR: ") + colors.white(message));
+    if (client != null) {
+        client.captureException(message);
+    }
 }
 
 export function debug(message: string) {
