695a8d3f82494bf7c96847cde76510bd9589c156
Innectic <innectic@gmail.com>
2016-11-06 12:06:14 -0800

    Add unix time to packet

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,12 @@ import { Server } from "./server";
 import { Config } from "./config";
 const config = new Config().config;
 
+import { Active } from "./active/active";
+
 const raven = require("raven");
+
+let active = new Active();
+active.watch();
 
 if (config.env === "prod") {
     const client = new raven.Client();
