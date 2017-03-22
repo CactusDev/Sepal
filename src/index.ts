279044ea82390d5c9ad414ea32ff3eeebb8c1b67
Innectic <innectic@gmail.com>
2017-03-22 07:59:45 -0700

    Organize index

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,84 @@
-
 import Config from "./configs/config";
 
 import { Rethink } from "./rethink";
 import { SepalSocket } from "./socket";
 import { RepeatHandler } from "./repeat";
 import { Logger } from "./logger";
+import { Redis } from "./redis";
 
-async function createSocket(rethink: Rethink) {
-    const socket: SepalSocket = new SepalSocket(Config, rethink);
-    await socket.create();
-
-    await startRepeat(socket, rethink);
+/**
+ * Init sentry
+ * 
+ */
+async function initLogger() {
+    await Logger.createSentry(Config.sentry);
 }
 
+/**
+ * Connect to rethink
+ * 
+ * @returns Rethink instance
+ */
+async function createRethink() {
+    const rethink: Rethink = new Rethink(Config);
+    await rethink.connect();
+    return rethink;
+}
+
+
+/**
+ * Connect to redis.
+ * 
+ * @returns Redis instance
+ */
+async function createRedis() {
+    const redis: Redis = new Redis(Config);
+    await redis.connect();
+    return redis;
+}
+
+
+/**
+ * Start repeats
+ * 
+ * @param {SepalSocket} socket SepalSocket instance
+ * @param {Rethink} rethink RethinkDB instance
+ */
 async function startRepeat(socket: SepalSocket, rethink: Rethink) {
     const repeat: RepeatHandler = new RepeatHandler(socket, rethink);
     await repeat.startRepeats();
 }
 
-async function createRethink() {
-    const rethink: Rethink = new Rethink(Config);
+/**
+ * Create Sepal's websocket and listen
+ * 
+ * @param {Rethink} rethink RethinkDB instance
+ * @returns SepalSocket instance
+ */
+async function createSocket(rethink: Rethink, redis: Redis) {
+    const socket: SepalSocket = new SepalSocket(Config, rethink);
+    await socket.create();
+
+    return socket;
+}
+
+
+/**
+ * Init everything
+ * 
+ */
+async function init() {
     try {
         await initLogger();
-        await rethink.connect();
-        await createSocket(rethink);
+
+        const rethink: Rethink = await createRethink();
+        const redis: Redis = await createRedis();
+        const socket: SepalSocket = await createSocket(rethink, redis);
+
+        await startRepeat(socket, rethink);
     } catch (e) {
-        console.error(e); // TODO: make this work with the logger
+        Logger.error(String(e));
     }
 }
 
-async function initLogger() {
-    await Logger.createSentry(Config.sentry);
-}
-
-createRethink();
+init();
