5d7c8e06ba673720226dca66c35a687c346525f7
Innectic <innectic@gmail.com>
2017-03-22 07:50:42 -0700

    Create redis handler

--- src/defs.d.ts
+++ src/defs.d.ts
@@ -12,10 +12,10 @@ interface IConfig {
         db: string;
     };
     redis: {
+        db: string;
         host: string;
         port: number;
         password: string;
-        db: number;
     };
     sentry: {
         dsn: string;
