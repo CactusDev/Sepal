5ab781810bf6af759028cbaf0a15e1d51320fcff
Innectic <innectic@gmail.com>
2017-10-24 23:34:52 -0700

    Remove literally every single file

--- src/packets/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./incoming/join";
-export * from "./outgoing/error";
-export * from "./outgoing/joined";
--- src/rethink/models/index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from "./alias";
-export * from "./command";
-export * from "./config";
-export * from "./quote";
-export * from "./repeat";
-export * from "./social";
-export * from "./trust";
